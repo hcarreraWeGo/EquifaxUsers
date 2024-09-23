e6758bc8e69fbce4bed76a26c5f25ece038edd5e
Stalin Vega <serviciosprofesionales.4@registrocivil.gob.ec>
2024-09-23 18:17:59 -0500

    ADD estructura

---
+++ src/app/app.module.ts
@@ -0,0 +1,60 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from "./shared/shared.module";
+// // for HttpClient import:
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
+// // for Router import:
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
+// // for Core import:
+import { LoadingBarModule } from '@ngx-loading-bar/core';
+
+import { CookieService } from 'ngx-cookie-service';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+
+import { OverlayModule } from '@angular/cdk/overlay';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    OverlayModule,
+    SharedModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+    }),
+
+    // for HttpClient use:
+    LoadingBarHttpClientModule,
+    // for Router use:
+    LoadingBarRouterModule,
+    // for Core use:
+    LoadingBarModule
+  ],
+  providers: [CookieService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

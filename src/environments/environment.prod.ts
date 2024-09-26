a59b9621cf70bdca8d708d7d361df11eb72f9135
Stalin Vega <serviciosprofesionales.4@registrocivil.gob.ec>
2024-09-26 17:14:10 -0500

    UPDATE ENVIORMENT

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: true,
+  apiUrl: 'http://3.9.236.139:3005/api/',  
+  enviocorreo:'http://3.141.185.126:81/api/correoLink',
+  authUsername: 'FIRMADORV3',
+  authPassword: 'super987'
 };

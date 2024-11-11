660a6ec0175672cb3a706a69ec28ce1eda9c1573
Stalin Vega <serviciosprofesionales.4@registrocivil.gob.ec>
2024-11-11 09:51:39 -0500

    UPDATE direccion de servidor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   apiUrl: 'http://3.9.236.139:3005/api/',
-  apiUrlBase:'http://localhost:3006/',
+  apiUrlBase:'http://3.9.236.139:3006/',
   enviocorreo:'http://3.141.185.126:81/api/correoLink',
   authUsername: 'FIRMADORV3',
   authPassword: 'super987',

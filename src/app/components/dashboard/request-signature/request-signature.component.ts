2694adbf33d213bb27c76a41760d1eb501250e05
Stalin Vega <serviciosprofesionales.4@registrocivil.gob.ec>
2024-11-12 12:27:55 -0500

    UPDATE validaciones formularios

--- src/app/components/dashboard/request-signature/request-signature.component.ts
+++ src/app/components/dashboard/request-signature/request-signature.component.ts
@@ -2,7 +2,8 @@ import { DashboardService } from './../dashboard.service';
 import { AlertService } from './../../../shared/components/alert/alert.service';
 import { ApiService } from './../../../shared/services/request-signature/api.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AlertServiceN } from 'src/app/shared/components/alert-n/alert.service';
 
 
 
@@ -16,32 +17,77 @@ export class RequestSignatureComponent implements OnInit {
   randomTextNumber: string | null = null;
 
 
-  constructor(private fb: FormBuilder, 
+  constructor(private fb: FormBuilder,
     private apiService: ApiService,
-    private alertService: AlertService,
-    private dashService:DashboardService) {
+    private alertService: AlertServiceN,
+    private dashService: DashboardService) {
     this.randomTextNumber = this.generateRandomTextNumber();
-   }
+  }
 
   ngOnInit() {
     this.solicitudForm = this.fb.group({
-      primerNombre: ['', Validators.required],
-      segundoNombre: [''],
-      primerApellido: ['', Validators.required],
-      segundoApellido: [''],
-      cedula: ['', Validators.required],
-      ciudad: ['', Validators.required],
-      provincia: ['', Validators.required],
+      primerNombre: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/)
+      ]
+      ], // Solo letras
+      segundoNombre: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/)
+      ]
+      ], // Solo letras
+      primerApellido: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/)
+      ]
+      ], // Solo letras
+      segundoApellido: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/)
+      ]
+      ], // Solo letras
+      cedula: ['', [
+        Validators.required,
+        Validators.pattern(/^\d+$/),
+        Validators.minLength(10),
+        Validators.maxLength(10),
+      ]
+      ], // Solo números, exactamente 10 dígitos
+      ciudad: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/)
+      ]
+      ], // Solo letras
+      provincia: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/)
+      ]
+      ], // Solo letras
       direccion: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      telefono: ['', Validators.required]
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]
+      ],// Validación de correo
+      telefono: ['', [
+        Validators.required,
+        Validators.pattern(/^\d+$/),
+        Validators.minLength(10),
+        Validators.maxLength(10)
+      ]
+      ] // Solo números, exactamente 10 dígitos
     });
   }
 
   async onSubmit() {
     if (this.solicitudForm.valid) {
       const formData = this.solicitudForm.value;
-  
+      // Genera un nuevo número aleatorio en cada envío
+      this.randomTextNumber = this.generateRandomTextNumber();
+      // ingreso de cliente
+      var nombres = formData.primerNombre + formData.segundoNombre;
+      var apellidos = formData.primerApellido + formData.segundoApellido;
+      await this.dashService.addCliente(nombres, apellidos, formData.cedula, this.randomTextNumber, formData.email, 1);
       // Preparamos el cuerpo para la API
       const requestBody = {
         "url": "https://enext.cloud/pre_equifax/links/generador/api/",
@@ -83,32 +129,28 @@ export class RequestSignatureComponent implements OnInit {
           ]
         }
       };
-  
+
       try {
         // Esperar la respuesta de la API
         const resp = await this.apiService.sendPostApiGenerica(requestBody);
         console.log('Respuesta de la API:', resp);
         //console.log("vercodigo",resp.codigo);
-        if (resp.codigo === "1"){
-          const data={
-            "link":resp.linkPrimerFirmante,
-            "correo":formData.email
+        if (resp.codigo === "1") {
+          const data = {
+            "link": resp.linkPrimerFirmante,
+            "correo": formData.email
           }
 
           // Envio de correo
           var envio = await this.apiService.envioLinkCorreo(data);
           this.alertService.showAlert('Correo enviado', 'success');
-          //Actualizamos numero de tramite
-          //await this.verifyService.addNumTramite(formData.cedula,this.randomTextNumber);
-          var nombres= formData.primerNombre+formData.segundoNombre;
-          var apellidos= formData.primerApellido+formData.segundoApellido;
-          await this.dashService.addCliente(nombres,apellidos,formData.cedula,this.randomTextNumber,formData.email,1);
+
           //console.log("Respuesta de envio de correo",envio.return);
           // Resetear el formulario
           this.solicitudForm.reset(); // Reinicia los campos del formulario
         }
-        else{
-          this.alertService.showAlert(envio.return, 'error');
+        else {
+          this.alertService.showAlert(envio.return, 'danger');
         }
       } catch (error) {
         console.error('Error al llamar a la API:', error);
@@ -117,11 +159,50 @@ export class RequestSignatureComponent implements OnInit {
   }
 
 
-  
+
 
   generateRandomTextNumber(): string {
     // Genera un número aleatorio de 6 dígitos y lo convierte a string
     const randomNumber = Math.floor(100000 + Math.random() * 900000);
     return randomNumber.toString(); // Convertir el número a string
+  }
+
+  // VALIDACIONES 
+  getCedulaErrorMessage(control: AbstractControl | null): string {
+    if (control?.hasError('required')) {
+      return 'La cédula es requerida.';
+    } else if (control?.hasError('pattern')) {
+      return 'La cédula debe contener solo números.';
+    } else if (control?.hasError('minlength') || control?.hasError('maxlength')) {
+      return 'La cédula debe tener exactamente 10 dígitos.';
+    }
+    return '';
+  }
+
+  getLetrasErrorMessage(control: AbstractControl | null): string {
+    if (control?.hasError('required')) {
+      return 'Este campo es requerido.';
+    } else if (control?.hasError('pattern')) {
+      return 'El campo solo debe contener letras.';
+    }
+    return '';
+  }
+  getTelefonoErrorMessage(control: AbstractControl | null): string {
+    if (control?.hasError('required')) {
+      return 'El telefono es requerido.';
+    } else if (control?.hasError('pattern')) {
+      return 'El telefono debe contener solo números.';
+    } else if (control?.hasError('minlength') || control?.hasError('maxlength')) {
+      return 'La telefono debe tener exactamente 10 dígitos.';
+    }
+    return '';
+  }
+  getCorreoErrorMessage(control: AbstractControl | null): string {
+    if (control?.hasError('required')) {
+      return 'El correo es requerido.';
+    } else if (control?.hasError('email')) {
+      return 'La correo es invalido.';
+    }
+    return '';
   }
 }
--- src/app/components/dashboard/verify-identity/verify-identity.component.ts
+++ src/app/components/dashboard/verify-identity/verify-identity.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { ApiService } from 'src/app/shared/services/request-signature/api.service';
-import { environment } from 'src/environments/environment';
 import { DashboardService } from '../dashboard.service';
 
 @Component({
@@ -10,41 +9,72 @@ import { DashboardService } from '../dashboard.service';
   templateUrl: './verify-identity.component.html',
   styleUrl: './verify-identity.component.scss'
 })
-export class VerifyIdentityComponent implements OnInit{
+export class VerifyIdentityComponent implements OnInit {
 
   solicitudForm: FormGroup;
   randomTextNumber: string | null = null;
 
 
-  constructor(private fb: FormBuilder, 
+  constructor(private fb: FormBuilder,
     private apiService: ApiService,
     private alertService: AlertService,
-    private dashService:DashboardService) {
+    private dashService: DashboardService) {
     this.randomTextNumber = this.generateRandomTextNumber();
-   }
+  }
 
   ngOnInit() {
     this.solicitudForm = this.fb.group({
-      primerNombre: ['', Validators.required],
-      segundoNombre: [''],
-      primerApellido: ['', Validators.required],
-      segundoApellido: [''],
-      cedula: ['', Validators.required],
-      // ciudad: ['', Validators.required],
-      // provincia: ['', Validators.required],
-      // direccion: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      telefono: ['', Validators.required]
+      primerNombre: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/)
+      ]
+      ], // Solo letras
+      segundoNombre: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/)
+      ]
+      ], // Solo letras
+      primerApellido: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/)
+      ]
+      ], // Solo letras
+      segundoApellido: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/)
+      ]
+      ], // Solo letras
+      cedula: ['', [
+        Validators.required,
+        Validators.pattern(/^\d+$/),
+        Validators.minLength(10),
+        Validators.maxLength(10),
+      ]
+      ], // Solo números, exactamente 10 dígitos
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]
+      ],// Validación de correo
+      telefono: ['', [
+        Validators.required,
+        Validators.pattern(/^\d+$/),
+        Validators.minLength(10),
+        Validators.maxLength(10)
+      ]
+      ] // Solo números, exactamente 10 dígitos
     });
   }
 
   async onSubmit() {
     if (this.solicitudForm.valid) {
+      // Genera un nuevo número aleatorio en cada envío
+      this.randomTextNumber = this.generateRandomTextNumber();
       const formData = this.solicitudForm.value;
       // AGREGAMOS A LA BDD
-      var nombres= formData.primerNombre+formData.segundoNombre;
-      var apellidos= formData.primerApellido+formData.segundoApellido;
-      await this.dashService.addCliente(nombres,apellidos,formData.cedula,this.randomTextNumber,formData.email,2);
+      var nombres = formData.primerNombre + formData.segundoNombre;
+      var apellidos = formData.primerApellido + formData.segundoApellido;
+      await this.dashService.addCliente(nombres, apellidos, formData.cedula, this.randomTextNumber, formData.email, 2);
       // Preparamos el cuerpo para la API
       const requestBody = {
         "url": "https://enext.cloud/firmador/links/generador/api/",
@@ -55,8 +85,8 @@ export class VerifyIdentityComponent implements OnInit{
         },
         "body": {
           "noTramite": this.randomTextNumber,
-          "certificado":{
-            "perfil":"001",
+          "certificado": {
+            "perfil": "001",
             "cedula": formData.cedula,
             "nombres": `${formData.primerNombre} ${formData.segundoNombre}`,
             "apellido1": formData.primerApellido,
@@ -69,17 +99,17 @@ export class VerifyIdentityComponent implements OnInit{
           }
         }
       };
-  
+
       try {
-       
+
         // Esperar la respuesta de la API
         const resp = await this.apiService.sendPostApiGenerica(requestBody);
         //console.log('Respuesta de la API:', resp);
         //console.log("vercodigo",resp.codigo);
-        if (resp.codigo === "1"){
-          const data={
-            "link":resp.link,
-            "correo":formData.email
+        if (resp.codigo === "1") {
+          const data = {
+            "link": resp.link,
+            "correo": formData.email
           }
 
           //console.log("Datos a enviar ",data);
@@ -89,7 +119,7 @@ export class VerifyIdentityComponent implements OnInit{
           // Resetear el formulario
           this.solicitudForm.reset(); // Reinicia los campos del formulario
         }
-        else{
+        else {
           this.alertService.showAlert(envio.return, 'error');
         }
       } catch (error) {
@@ -99,7 +129,7 @@ export class VerifyIdentityComponent implements OnInit{
   }
 
 
-  
+
 
   generateRandomTextNumber(): string {
     // Genera un número aleatorio de 6 dígitos y lo convierte a string
@@ -107,4 +137,43 @@ export class VerifyIdentityComponent implements OnInit{
     return randomNumber.toString(); // Convertir el número a string
   }
 
+
+  // VALIDACIONES 
+  getCedulaErrorMessage(control: AbstractControl | null): string {
+    if (control?.hasError('required')) {
+      return 'La cédula es requerida.';
+    } else if (control?.hasError('pattern')) {
+      return 'La cédula debe contener solo números.';
+    } else if (control?.hasError('minlength') || control?.hasError('maxlength')) {
+      return 'La cédula debe tener exactamente 10 dígitos.';
+    }
+    return '';
+  }
+
+  getLetrasErrorMessage(control: AbstractControl | null): string {
+    if (control?.hasError('required')) {
+      return 'Este campo es requerido.';
+    } else if (control?.hasError('pattern')) {
+      return 'El campo solo debe contener letras.';
+    }
+    return '';
+  }
+  getTelefonoErrorMessage(control: AbstractControl | null): string {
+    if (control?.hasError('required')) {
+      return 'El telefono es requerido.';
+    } else if (control?.hasError('pattern')) {
+      return 'El telefono debe contener solo números.';
+    } else if (control?.hasError('minlength') || control?.hasError('maxlength')) {
+      return 'La telefono debe tener exactamente 10 dígitos.';
+    }
+    return '';
+  }
+  getCorreoErrorMessage(control: AbstractControl | null): string {
+    if (control?.hasError('required')) {
+      return 'El correo es requerido.';
+    } else if (control?.hasError('email')) {
+      return 'La correo es invalido.';
+    }
+    return '';
+  }
 }
